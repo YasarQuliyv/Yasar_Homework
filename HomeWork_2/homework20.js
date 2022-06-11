1f226eae385fb8fd8e150d7ed7475eb9ca88f2ce
YasarQuliyv <106619253+YasarQuliyv@users.noreply.github.com>
2022-06-12 01:20:31 +0400

    Homework #2

--- HomeWork_2/homework20.js
+++ HomeWork_2/homework20.js
@@ -1,20 +1,17 @@
-
 function ceasarCipher(str, k) {
-    let arr = [];
+    let arr = "";
     const alfabe = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'];
     for (let i = 0; i < alfabe.length; i++) {
         for (let j = 0; j < alfabe.length; j++) {
-            if (str.charCodeAt(i) == alfabe[j]) {
-                arr.push(alfabe[j + k]);
-
-
+            if (str.charAt(i) == alfabe[j]) {
+                arr += alfabe[j + k + 2];
             }
         }
-        return arr;
     }
-   
-
+    return arr;
 }
 
-console.log(ceasarCipher(['Salam',1]));
+console.log(ceasarCipher('memmedeli', 4)); // cavab: EXXEGOEXSRGI
+
+
 
